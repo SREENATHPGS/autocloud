a6a92d1cbb6ec0f0e202f227cf0f13eb980c9ea5
SREENATH P G <sreenath@shiftbytes.co.in>
2023-09-12 11:33:49 +0530

    Update init_setup_machine.sh
    Added funciton names so that packages can be installed by calling functions.

--- install_scripts/init_setup_machine.sh
+++ install_scripts/init_setup_machine.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install -yy wget zip unzip vim tree htop awscli
-wget -O - https://raw.githubusercontent.com/SREENATHPGS/autocloud/main/install_scripts/install_docker.sh | bash
+function basic_packages() {
+  sudo apt update
+  sudo apt install -yy wget zip unzip vim tree htop awscli
+}
+
+function install_docker() {
+  wget -O - https://raw.githubusercontent.com/SREENATHPGS/autocloud/main/install_scripts/install_docker.sh | bash
+}
+
+
+function install_all() {
+  basic_packages
+  install_docker
+}
